84cd961532a9345e793bdab405101f1387868b4c
Erik van Kempen <erik@inclino.nl>
2016-11-11 21:51:06 +0100

    Full size demo mode

--- Balestra/Balestra.ino
+++ Balestra/Balestra.ino
@@ -8,17 +8,18 @@
 #define FLECHE 4
 
 // Set definitions for LED strip
-#define NUM_LEDS 30
+#define NUM_LEDS 120
 #define NUM_LEDS_PER_METER 30
 #define DATA_PIN 6
 CRGB leds[NUM_LEDS];
 
+/* Clear the full length of the led strip */
 void clear () {
   fill_solid( &(leds[0]), NUM_LEDS, CRGB::Black );
   FastLED.show();
 }
 
-void position ( int x, int r, bool hb ) {
+void position ( int x, int r ) {
   // Fix over- and underflow
   if ( x - r < -1 ) {
     x = 0;
@@ -30,22 +31,14 @@ void position ( int x, int r, bool hb ) {
     r = 1;
   }
 
-  // Heart beat animation
-  if ( hb ) {
-    leds[x] = CRGB::Red;
-    for ( int i = 1; i <= r; i++ ) {
-      leds[x-i] = CRGB( 255*(r-i)/r, 0, 0);
-      leds[x+i] = CRGB( 255*(r-i)/r, 0, 0);
-    }
-  } else if ( !hb ) {
-    leds[x] = CRGB::Red;
-    for ( int i = 1; i <= r; i++ ) {
-      leds[x-i] = CRGB( 255*(r-i)/r, 0, 0);
-      leds[x+i] = CRGB( 255*(r-i)/r, 0, 0);
-    }
+  
+  leds[x] = CRGB::Blue;
+  for ( int i = 1; i < r; i++ ) {
+    leds[x-i] = CRGB( 255*(r-i)/r, 0, 0);
+    leds[x+i] = CRGB( 255*(r-i)/r, 0, 0);
   }
   FastLED.show();
-  delay(25);
+  delay(5);
 }
 
 void move (int moveType, int speed, int start) {
@@ -67,8 +60,8 @@ void move (int moveType, int speed, int start) {
 
 void setup() {
   delay(2000);
-  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  //FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
+  //FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
 
@@ -76,14 +69,15 @@ void setup() {
 // your leds.
 void loop() {
   for ( int i = 0; i < NUM_LEDS; i++ ) {
-    position(i, 2, false);
-    delay(25);
+    position(i, 2);
+    position(NUM_LEDS - i, 4);
+    //delay(25);
     clear();
   }
   
-  for ( int i = NUM_LEDS; i > 0; i-- ) {
-    position(i, 2, false);
-    delay(25);
+  /*for ( int i = NUM_LEDS; i > 0; i-- ) {
+    position(i, 3);
+    //delay(25);
     clear();
-  }
+  }*/
 }
